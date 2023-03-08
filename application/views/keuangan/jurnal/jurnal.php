d63245113a56067d710d1aea6d53c8c920207ef1
SECONDTA ARDIANSYAH <idnssquad300@gmail.com>
2023-03-08 09:29:36 +0700

    UI Jurnal Penyesuaian

--- application/views/keuangan/jurnal/jurnal.php
+++ application/views/keuangan/jurnal/jurnal.php
@@ -24,12 +24,14 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Akun</h1>
+                            <h1>Jurnal Penyesuaian</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="<?php echo base_url('Akademik/') ?>"><?php echo $this->session->userdata('level') ?></a></li>
-                                <li class="breadcrumb-item active">Akun</li>
+                                <li class="breadcrumb-item"><a
+                                        href="<?php echo base_url('Keuangan/') ?>"><?php echo $this->session->userdata('level') ?></a>
+                                </li>
+                                <li class="breadcrumb-item active">Jurnal Penyesuaian</li>
                             </ol>
                         </div>
                     </div>
@@ -37,76 +39,59 @@
             </section>
 
             <section class="content">
-                <div class="container-fluid bg-white shadow">
-                    <div class="row mx-2 pt-3 d-flex justify-content-between">
-                        <div class="col-2 col-sm-6 ">
-                            <div class="form-group d-flex flex-row " style="width: fit-content;">
-                                <div class="mt-2 mx-1">
-                                    <h4>Data Akun</h4>
+                <div class="container-fluid bg-white shadow p-3">
+                    <div class="row">
+                        <div class="col-6 h5">Input Jurnal</div>
+                        <div class="col-6"><button type="submit" class="btn btn-primary float-right">Lihat Data</button>
+                        </div>
+                        <div class="col-12">
+                            <div class="row mt-3">
+                                <div class="col-4 text-right font-weight-bold mt-1">Uraian</div>
+                                <div class="col-5">
+                                    <input type="text" name="uraian" class="form-control" placeholder="Uraian">
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 d-flex justify-content-end align-self-start">
-                            <a
-                                <button type="button" class="btn btn-success">
-                                    <i class="fa fa-plus pr-2"></i>Tambah
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card-body">
-                                <table id="datasiswa-table" class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>ID Akun</th>
-                                            <th>Nama Akun</th>
-                                            <th>Jenis Akun</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                1
-                                            </td>
-                                            <td>
-                                                101
-                                            </td>
-                                            <td>
-                                                Kas
-                                            </td>
-                                            <td>
-                                                Aset
-                                            </td>
-                                            <td class="text-center">
-                                                <a class="btn btn-primary btn-sm">
-                                                    <i class="fa fa-edit"></i>Edit</a>
-                                                <button class="btn btn-danger btn-sm">
-                                                    <i class="fa fa-trash"></i>Hapus</button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                            <div class="row mt-3">
+                                <div class="col-4 text-right font-weight-bold mt-1">Akun Debet</div>
+                                <div class="col-5">
+                                    <select name="akun_debit" class="custom-select custom-select-md">
+                                        <option value="adasdad">AWIKWOK</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-4 text-right font-weight-bold mt-1">Akun Kredit</div>
+                                <div class="col-5">
+                                    <select name="akun_debit" class="custom-select custom-select-md">
+                                        <option value="adasdad">WOKWOKWOK</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-4 text-right font-weight-bold mt-1">Nominal</div>
+                                <div class="col-5">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Rp</span>
+                                        </div>
+                                        <input type="number" name="nominal" class="form-control" placeholder="Nominal">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-4 text-right font-weight-bold">
+                                </div>
+                                <div class="col-5">
+                                    <button type="submit" class="btn btn-primary">Input</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </section>
         </div>
-        </section>
-    </div>
     </div>
     <?php $this->load->view('keuangan/style/js') ?>
-    <script>
-        function hapus(id) {
-            var yes = confirm('Yakin Di Hapus?');
-            if (yes == true) {
-                window.location.href = "<?php echo base_url('Akademik/hapus_guru/') ?>" + id;
-            }
-        }
-    </script>
 </body>
 
 </html>
