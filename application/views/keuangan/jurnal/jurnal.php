887ea0b444e5c0d2083634e36f7f655e7fee231a
Rizqi Ramandika <rizqiramandika96@gmail.com>
2023-03-09 14:53:22 +0700

    Update > Keuangan > Jurnal Penyesuaian

--- application/views/keuangan/jurnal/jurnal.php
+++ application/views/keuangan/jurnal/jurnal.php
@@ -42,56 +42,123 @@
                 <div class="container-fluid bg-white shadow p-3">
                     <div class="row">
                         <div class="col-6 h5">Input Jurnal</div>
-                        <div class="col-6"><button type="submit" class="btn btn-primary float-right">Lihat Data</button>
+                        <div class="col-6">
+                            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#modal_lihat_data">
+                                Lihat Data
+                            </button>
                         </div>
                         <div class="col-12">
-                            <div class="row mt-3">
-                                <div class="col-4 text-right font-weight-bold mt-1">Uraian</div>
-                                <div class="col-5">
-                                    <input type="text" name="uraian" class="form-control" placeholder="Uraian">
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="col-4 text-right font-weight-bold mt-1">Akun Debet</div>
-                                <div class="col-5">
-                                    <select name="akun_debit" class="custom-select custom-select-md">
-                                        <option value="adasdad">AWIKWOK</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="col-4 text-right font-weight-bold mt-1">Akun Kredit</div>
-                                <div class="col-5">
-                                    <select name="akun_debit" class="custom-select custom-select-md">
-                                        <option value="adasdad">WOKWOKWOK</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="col-4 text-right font-weight-bold mt-1">Nominal</div>
-                                <div class="col-5">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="basic-addon1">Rp</span>
-                                        </div>
-                                        <input type="number" name="nominal" class="form-control" placeholder="Nominal">
+                            <form action="<?php echo base_url('Keuangan/aksi_input_jurnal') ?>" method="post">
+                                <div class="row mt-3">
+                                    <div class="col-4 text-right font-weight-bold mt-1">Uraian</div>
+                                    <div class="col-5">
+                                        <input type="text" name="uraian" class="form-control" placeholder="Uraian">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="col-4 text-right font-weight-bold">
+                                <div class="row mt-3">
+                                    <div class="col-4 text-right font-weight-bold mt-1">Akun Debit</div>
+                                    <div class="col-5">
+                                        <select name="id_akun" class="custom-select custom-select-md">
+                                            <option selected>-- Pilih Akun --</option>
+                                            <?php foreach ($data_akun as $data): ?>
+                                                <option value="<?php echo $data->id_akun ?>"><?php echo $data->nama_akun ?></option>
+                                            <?php endforeach ?>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div class="col-5">
-                                    <button type="submit" class="btn btn-primary">Input</button>
+                                <div class="row mt-3">
+                                    <div class="col-4 text-right font-weight-bold mt-1">Nominal</div>
+                                    <div class="col-5">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" id="basic-addon1">Rp</span>
+                                            </div>
+                                            <input type="number" name="nominal" class="form-control" placeholder="Nominal">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="row mt-3">
+                                    <div class="col-4 text-right font-weight-bold">
+                                    </div>
+                                    <div class="col-5">
+                                        <input type="hidden" value="<?php echo $dt->email ?>" name="pencatat">
+                                        <button type="submit" class="btn btn-primary">Input</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </section>
         </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="modal_lihat_data" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Data Transaksi</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body pb-1">
+                        <div class="box">
+                            <!-- /.box-header -->
+                            <div class="box-body">
+                                <table id="data-table2" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Tanggal</th>
+                                            <th>Uraian</th>
+                                            <th>Pencatat</th>
+                                            <th>Akun</th>
+                                            <th>Nominal</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $id = 0; foreach($data_jurnal as $data): $id++ ?>
+                                            <tr>
+                                                <td><?php echo $id ?></td>
+                                                <td><?php echo $data->waktu ?></td>
+                                                <td><?php echo $data->uraian ?></td>
+                                                <td><?php echo $data->pencatat ?></td>
+                                                <td><?php echo tampil_nama_akun_transaksi($data->id_akun) ?></td>
+                                                <td><?php echo $data->nominal ?></td>
+                                                <td>
+                                                    <button onClick="hapus(<?php echo $data->id_jurnal ?>)" class="btn btn-danger btn-sm">
+                                                        <i class="fa fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer d-flex justify-content-between">
+                        <div></div>
+                        <button type="button" class="btn btn-secondary" onclick="kembali()"
+                            data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     <?php $this->load->view('keuangan/style/js') ?>
+    <script>
+    function hapus(id) {
+         var yes = confirm('Yakin Di Hapus?');
+        if (yes == true) {
+            window.location.href = "<?php echo base_url('Keuangan/hapus_jurnal/') ?>" + id;
+        }
+    }
+    </script>
 </body>
 
 </html>
