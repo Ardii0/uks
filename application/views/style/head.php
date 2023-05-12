6cadb3aa12f66961e4981a7d14a321fdc6bd79a4
Ardi <mhmmdard01@gmail.com>
2023-05-12 14:17:35 +0700

    Update: Database, Style & Alert Kegiatan,Condition Status Pasien, Fix Tambah Periksa, Fix Select2

--- application/views/style/head.php
+++ application/views/style/head.php
@@ -2,6 +2,8 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/fontawesome-free/css/all.min.css'); ?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('builder/dist/css/adminlte.min.css'); ?>">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/daterangepicker/daterangepicker.css'); ?>">
@@ -10,6 +12,7 @@
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/datatables-responsive/css/responsive.bootstrap4.min.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/datatables-buttons/css/buttons.bootstrap4.min.css'); ?>">
   <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url('builder/dist/css/adminlte.min.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/select2/css/select2.min.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('builder/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css'); ?>">
   <!-- Theme style -->
