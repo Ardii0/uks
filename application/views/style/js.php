379097bd97ce935623e5fbaffc8d9c87882c2747
saniadv <fzats44@gmail.com>
2023-05-12 14:21:01 +0700

    styling in table

--- application/views/style/js.php
+++ application/views/style/js.php
@@ -66,6 +66,14 @@ $(function() {
         "lengthChange": true,
         "autoWidth": false,
     }).buttons().container().appendTo('#data-table2_wrapper .col-md-6:eq(0)');
+
+    $("#table3").DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "bPaginate": false,
+        "bFilter": false,
+        "autoWidth": false,
+    }).buttons().container().appendTo('#table_wrapper .col-md-6:eq(0)');
 });
 </script>
 <!-- Select2 -->
