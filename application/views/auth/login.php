47427a3a63b3dee71c90487336db26639c571369
malaFillatunnida <fillatunnida@gmail.com>
2023-05-12 10:13:57 +0700

    update style login & register

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -16,13 +16,6 @@
         background: #eaeff4;
     }
 
-    .wrapper {
-        width: 100%;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
-
     .container {
         position: relative;
         display: flex;
@@ -76,7 +69,7 @@
         background: #4CAF50;
         -webkit-clip-path: polygon(98% 17%, 100% 34%, 98% 51%, 100% 68%, 98% 84%, 100% 100%, 0 100%, 0 0, 100% 0);
         clip-path: polygon(98% 17%, 100% 34%, 98% 51%, 100% 68%, 98% 84%, 100% 100%, 0 100%, 0 0, 100% 0);
-        margin-left: -7px;
+        margin-left: -8px;
     }
 
     .login-text {
@@ -132,63 +125,67 @@
 </head>
 
 <body>
-    <div class="wrapper mt-4">
-        <div class="container ">
-            <div class="col-left">
-                <div class="login-text">
-                    <div class="header">
-                        <h3 class="mt-2">UKS SMP N 1 SEMARANG</h3>
-                        <h5 class="mt-2">Motto</h5>
-                        <h6 class="text-center "><b> TERDEPAN</b></h6>
-                        <p class="text-center mt-2">“ Terintegritas Dedikasi Empati Profesional Akurat Normatif ”</p>
-                        <h5>Visi</h5>
-                        <p class="text-center ">“ Luhur Budi, Cerdas, Berprestasi ”</p>
-                        <h5>Misi</h5>
+    <div class="min-vh-100 d-flex justify-content-center align-items-center">
+        <div class="wrapper">
+            <div class="container ">
+                <div class="col-left">
+                    <div class="login-text">
+                        <div class="header">
+                            <h3 class="mt-2">UKS SMP N 1 SEMARANG</h3>
+                            <h5 class="mt-2">Motto</h5>
+                            <h6 class="text-center "><b> TERDEPAN</b></h6>
+                            <p class="text-center mt-2">“ Terintegritas Dedikasi Empati Profesional Akurat Normatif ”
+                            </p>
+                            <h5>Visi</h5>
+                            <p class="text-center ">“ Luhur Budi, Cerdas, Berprestasi ”</p>
+                            <h5>Misi</h5>
+                        </div>
+                        <ol type="1" style=" margin-right: 20px">
+                            <li>Menyelenggarakan pendidikan yang lebih baik dan bermutu sehingga dapat menciptakan
+                                peserta
+                                didik yang komprehensif dan kompetitif, berpenghayatan terhadap ajaran yang dianut dan
+                                berbudi pekerti luhur sehingga siswa mampu mengaktualisasikan dalam kehidupannya
+                                sehari-hari.</li>
+                            <li>Melaksanakan kegiatan belajar mengajar yang intensif, efektif, dan efisien, serta
+                                memberikan
+                                bimbingan yang maksimal kepada peserta didik sehingga mampu berkembang secara optimal
+                                sesuai
+                                dengan potensi yang dimiliki.</li>
+                            <li>Melaksanakan kegiatan ekstrakurikuler secara intensif sehingga dapat memupuk bakat dan
+                                minat
+                                yang dimiliki para peserta didik.</li>
+                            <li>Melaksanakan kegiatan jam tambahan secara intensif, ekstensif secara terstruktur dan
+                                terprogram.</li>
+                            <li>Melaksanakan kegiatan lain yang mendukung dan berkaitan dengan kegiatan sekolah yang
+                                lebih
+                                baik, transparan, akuntabel, dan demokratis.</li>
+                        </ol>
                     </div>
-                    <ol type="1" style=" margin-right: 20px">
-                        <li>Menyelenggarakan pendidikan yang lebih baik dan bermutu sehingga dapat menciptakan
-                            peserta
-                            didik yang komprehensif dan kompetitif, berpenghayatan terhadap ajaran yang dianut dan
-                            berbudi pekerti luhur sehingga siswa mampu mengaktualisasikan dalam kehidupannya
-                            sehari-hari.</li>
-                        <li>Melaksanakan kegiatan belajar mengajar yang intensif, efektif, dan efisien, serta
-                            memberikan
-                            bimbingan yang maksimal kepada peserta didik sehingga mampu berkembang secara optimal
-                            sesuai
-                            dengan potensi yang dimiliki.</li>
-                        <li>Melaksanakan kegiatan ekstrakurikuler secara intensif sehingga dapat memupuk bakat dan
-                            minat
-                            yang dimiliki para peserta didik.</li>
-                        <li>Melaksanakan kegiatan jam tambahan secara intensif, ekstensif secara terstruktur dan
-                            terprogram.</li>
-                        <li>Melaksanakan kegiatan lain yang mendukung dan berkaitan dengan kegiatan sekolah yang
-                            lebih
-                            baik, transparan, akuntabel, dan demokratis.</li>
-                    </ol>
                 </div>
-            </div>
-            <div class="col-right">
-                <div class="login-form">
-                    <h5>
-                        <img src="<?php echo base_url('assets/logo_login.png'); ?>" width="280" height="280"
-                            class="d-inline-block align-text-top me-3">
-                    </h5>
-                    <h3>Login</h3>
-                    <form action="<?php echo base_url('Login/aksi_login')?>" method="post">
-                        <p>
-                            <input type="text" name="username" placeholder="Username" required>
-                        </p>
-                        <p style="display: flex">
-                            <input type="password" name="password" placeholder="Password" id="myPass">
-                            <span id="showPass">
-                                <i class="fa fa-eye-slash" aria-hidden="true"></i>
-                                <i class="fa fa-eye" aria-hidden="true" style="display:none;"></i>
-                            </span>
-                        </p>
-                        <p>
-                            <button class="btn btn-success w-100" type="submit" style="height: 58px">Sign In</button>
-                        </p>
-                    </form>
+                <div class="col-right">
+                    <div class="login-form">
+                        <h5>
+                            <img src="<?php echo base_url('assets/logo_login.png'); ?>" width="280" height="280"
+                                class="d-inline-block align-text-top me-3">
+                        </h5>
+                        <h3>Login</h3>
+                        <form action="<?php echo base_url('Login/aksi_login')?>" method="post">
+                            <p>
+                                <input type="text" name="username" placeholder="Username" required>
+                            </p>
+                            <p style="display: flex">
+                                <input type="password" name="password" placeholder="Password" id="myPass">
+                                <span id="showPass">
+                                    <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                    <i class="fa fa-eye" aria-hidden="true" style="display:none;"></i>
+                                </span>
+                            </p>
+                            <p>
+                                <button class="btn btn-success w-100" type="submit" style="height: 58px">Sign
+                                    In</button>
+                            </p>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
