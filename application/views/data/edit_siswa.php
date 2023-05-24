2b1023240ed776111fcf2e59cefb2d57577f336f
malaFillatunnida <fillatunnida@gmail.com>
2023-05-24 14:36:43 +0700

    edit siswa

--- application/views/data/edit_siswa.php
+++ application/views/data/edit_siswa.php
@@ -58,8 +58,8 @@
                                                         <div class="custom-file mb-3">
                                                             <input type="file" class="custom-file-input" id="customFile"
                                                                 name="foto">
-                                                            <label class="custom-file-label" for="customFile">Pilih
-                                                                File</label>
+                                                            <label class="custom-file-label"
+                                                                for="customFile"><?php echo $datas->foto ?></label>
                                                         </div>
                                                     </div>
                                                     <div class="form-group col-sm-6">
