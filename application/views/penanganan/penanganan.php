96b69b06253ae8781341226548ade3b999d45683
saniadv <fzats44@gmail.com>
2023-05-26 10:44:51 +0700

    sweetalert in delete

--- application/views/penanganan/penanganan.php
+++ application/views/penanganan/penanganan.php
@@ -148,12 +148,14 @@
             confirmButtonText: ' Ya hapus!'
         }).then((result) => {
             if (result.isConfirmed) {
-                window.location.href = "<?php echo base_url('penanganan/hapus_penanganan/')?>" + id;
+                
                 Swal.fire({
                     icon: 'success',
                     title: 'Berhasil Dihapus',
                     showConfirmButton: false,
-                    timer: 1500
+                    timer: 1250
+                }).then(function() {
+                    window.location.href = "<?php echo base_url('penanganan/hapus_penanganan/')?>" + id;
                 })
 
             }
